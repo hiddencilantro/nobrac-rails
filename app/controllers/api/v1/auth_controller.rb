fa7b2beddbf941a13b730e38bfad522698539432
hiddencilantro <stevekim1109@gmail.com>
2022-03-18 16:58:24 -0400

    changed error handling for login (now throwing 401 and 404 separately for wrong password/no user

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -1,11 +1,15 @@
 class Api::V1::AuthController < ApplicationController
     def create
         user = User.find_by(email: user_login_params[:email])
-        if user && user.authenticate(user_login_params[:password])
-            token = encode_token({user_id: user.id})
-            render json: { user: UserSerializer.new(user), jwt: token }, status: :created
+        if user
+            if user.authenticate(user_login_params[:password])
+                token = encode_token({user_id: user.id})
+                render json: { user: UserSerializer.new(user), jwt: token }, status: :created
+            else
+                render json: { message: 'Password is incorrect' }, status: :unauthorized
+            end
         else
-            render json: { message: 'Invalid username or password' }, status: :unauthorized
+            render json: { message: "Email doesn't exist" }, status: :not_found
         end
     end
 
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,14 @@
 class ApplicationController < ActionController::API
     rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
-    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+    # rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
     def render_unprocessable_entity_response(exception)
-        render json: exception.record.errors, status: :unprocessable_entity
+        render json: exception.record.errors.full_messages, status: :unprocessable_entity
     end
 
-    def render_not_found_response(exception)
-        render json: { error: exception.message }, status: :not_found
-    end
+    # def render_not_found_response(exception)
+    #     render json: { error: exception.message }, status: :not_found
+    # end
 
     def encode_token(payload)
         JWT.encode(payload, ENV['JWT_SECRET'])
