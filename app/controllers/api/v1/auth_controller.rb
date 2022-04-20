3a1bc43273d4b54acefdc321f97171b451399091
hiddencilantro <stevekim1109@gmail.com>
2022-04-20 14:19:23 -0400

    added 400 response for blank email field

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -1,15 +1,19 @@
 class Api::V1::AuthController < ApplicationController
     def create
-        user = User.find_by(email: user_login_params[:email])
-        if user
-            if user.authenticate(user_login_params[:password])
-                token = encode_token({user_id: user.id})
-                render json: { user: UserSerializer.new(user), jwt: token }, status: :created
-            else
-                render json: { message: 'Password is incorrect' }, status: :unauthorized
-            end
+        if user_login_params[:email].blank?
+            render json: { message: "Email can't be blank" }, status: :bad_request
         else
-            render json: { message: "Email doesn't exist" }, status: :not_found
+            user = User.find_by(email: user_login_params[:email])
+            if user
+                if user.authenticate(user_login_params[:password])
+                    token = encode_token({user_id: user.id})
+                    render json: { user: UserSerializer.new(user), jwt: token }, status: :created
+                else
+                    render json: { message: 'Password is incorrect' }, status: :unauthorized
+                end
+            else
+                render json: { message: "Email doesn't exist" }, status: :not_found
+            end
         end
     end
 
